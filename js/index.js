1091b8b15649ebbf0d6a9217ad0601e56fde188d
NamrataVivek <132909704+NamrataVivek@users.noreply.github.com>
2023-05-15 11:26:31 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,15 +4,33 @@ let b = 5;
 let c;
 
 // alert( "description of alerted_value: " + alerted_value );
-let output = `
+let print = `
 let a = 3;
 let b = 5;
 let c;
 --------
-a+b= ${a + b}
+
+a + b = ${a + b}
+a - b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}
+a % b = ${a % b}
+a += b = ${a += b}
+a -= b = ${a -= b}
+a *= b = ${a *= b}
+a /= b = ${a /= b}
+a %= b = ${a %= b}
+a == b = ${a == b}
+a != b = ${a != b}
+a > b = ${a > b}
+a < b = ${a < b}
+!a && !c = ${!a && !c}
+!a || !c = ${!a || !c}
 
 `;
-alert(output);
+
+alert(print);
+
 /*
 a + b
 a - b
@@ -31,3 +49,12 @@ a < b
 !a && !c 
 !a || !c 
 */
+
+//TASK 2
+
+let first_name= "Namrata";
+let last_name= "Kambli";
+let email= "kamb0030@algonquinlive.com";
+let output= `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+alert(output);
