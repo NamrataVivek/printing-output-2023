700ac5e7d155886ce0022c283a77c11bc9df947e
NamrataVivek <132909704+NamrataVivek@users.noreply.github.com>
2023-05-15 09:38:40 -0400

    Create index.js
    JavaScript File created, and JS content added

---
+++ js/index.js
@@ -0,0 +1,33 @@
+//TASK 1 //string 
+let a = 3;
+let b = 5;
+let c;
+
+// alert( "description of alerted_value: " + alerted_value );
+let output = `
+let a = 3;
+let b = 5;
+let c;
+--------
+a+b= ${a + b}
+
+`;
+
+/*
+a + b
+a - b
+a * b
+a / b
+a % b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/
